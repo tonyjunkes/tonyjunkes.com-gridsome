4d475df927312061324014c5e27e2f6a8f77ae0a
Tony Junkes <tony@tonyjunkes.com>
2020-04-10 02:23:13 -0400

    tailwindcss downgrade: take 2

--- gridsome.config.js
+++ gridsome.config.js
@@ -19,17 +19,6 @@ module.exports = {
   siteUrl: 'https://tonyjunkes.com',
   plugins: [
     {
-      use: 'gridsome-plugin-tailwindcss',
-      options: {
-        tailwindConfig: './tailwind.config.js',
-        purgeConfig: {},
-        presetEnvConfig: {},
-        shouldPurge: true,
-        shouldImport: true,
-        shouldTimeTravel: true
-      }
-    },
-    {
       use: '@gridsome/source-filesystem',
       options: {
         path: 'blog/**/*.md',
